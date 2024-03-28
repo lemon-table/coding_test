3431c597cb1544b8e6365ddaf61ca95cdd2cc882
=lemon-table <=>
2024-03-28 20:31:44 +0900

    fix: 두수의차.java 답안 수정

--- src/java/두수의차.java
+++ src/java/두수의차.java
@@ -3,7 +3,17 @@ package src.java;
 public class 두수의차 {
 
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        두수의차 instance = new 두수의차();
+
+        int result = instance.solution(10, 5); // 예시로 10과 5를 사용
+        System.out.println("두 수의 차: " + result);
     }
+
+        // solution 메서드
+        public int solution(int num1, int num2) {
+            return num1 - num2;
+        }
+    
     
 }
